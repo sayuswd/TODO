65102a3e0480d8a9c6262b3eb9296d0a1bc5d866
sayuswd <sisi1013hola@yahoo.co.jp>
2019-04-21 22:25:47 +0900

    JS修正

--- todo.js
+++ todo.js
@@ -1,35 +1,41 @@
-  //文字が入力されたときに実行
-  $('#textinput').keyup(function () {
-    //入力した文字を取得
-    const input_textvalue = $('#textinput').val();
+//データを取得する　Model機能
+class Model {
+  constructor() {
+    this.todoLists = [];
+  }
+}
+let todoModel = new Model();
 
-    //登録ボタンが押されたら追加
-    $('.addbutton').click(function () {
-      //もし、入力されていたら表示
+//文字を表示する　　View機能
+function viewModel(todo_text) {
+  //HTMLに表示する機能
+  let inputDate = todo_text;
 
-      if (input_textvalue != "") {
-      
-      const nextvalue1 = $(".todo_text01").val();
-      const nextvalue2 = $(".todo_text02").val();
-      const nextvalue3 = $(".todo_text03").val();
+  $(inputDate).text(todoModel.todoLists[0]);
 
-        //入力した文字を表示 NO.1
-        $(".todo_text01").text(input_textvalue);
-        //文字の検索
-        
+}
 
-        if (nextvalue2 != nextvalue1) {
-          //入力した文字を表示 NO.2
-          $(".todo_text02").text(input_textvalue);
-        }
+//登録ボタンが押されたら追加
+$('.addbutton').click(function () {
+  //入力した文字を取得
+  let input_value = $('#textinput').val();
 
-        if (nextvalue3 != nextvalue2 || nextvalue3 != nextvalue1 ) {
-          //入力した文字を表示 NO.3
-          $(".todo_text03").text(input_textvalue);
-        }
-      } else {
-        return;
-      }
+  //もし、入力されていたら表示
+  if (input_value != "") {
 
-    });
-  });
+    //Modelにinputされたデータを準備する
+    todoModel.todoLists.push(input_value);
+
+    viewModel(".todo_text01");
+
+
+  } else {
+    return;
+  }
+
+});
+
+//文字が入力されたときに実行
+$('#textinput').keyup(function () {
+
+});
