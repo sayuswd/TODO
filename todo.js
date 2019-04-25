7026bfbef9816b32970e1f924647546b0b15e053
sayuswd <sisi1013hola@yahoo.co.jp>
2019-04-25 22:09:28 +0900

    .push修正中（仮コミット）

--- todo.js
+++ todo.js
@@ -10,16 +10,17 @@ let todoModel = new Model();
 //文字を表示する　　View機能
 function viewModel() {
   //HTMLに表示する機能
-    
+
+  if (todoModel.todoLists.length > 0) {
     const htmlcode01 = '<label><input type="checkbox"/><span class="checkbox-icon"></span><p class="todo_text01"></p></label>';
     $("#todo_text").html(htmlcode01);
     $(".todo_text01").text(todoModel.todoLists[0]);
-
+  }
   if (todoModel.todoLists.length > 1) {
     const htmlcode02 = '<label><input type="checkbox"/><span class="checkbox-icon"></span><p class="todo_text02"></p></label>';
     $("#todo_text").append(htmlcode02);
     $(".todo_text02").text(todoModel.todoLists[1]);
-  } 
+  }
   if (todoModel.todoLists.length > 2) {
     const htmlcode03 = '<label><input type="checkbox"/><span class="checkbox-icon"></span><p class="todo_text03"></p></label>';
     $("#todo_text").append(htmlcode03);
@@ -33,37 +34,39 @@ $('.addbutton').click(function () {
   let input_value = $('#textinput').val();
 
   //もし、入力されていたら表示
-  if (input_value != "") {
+  if (todoModel.todoLists.length == 0) {
 
-    //Modelにinputされたデータを格納する
-    todoModel.todoLists.push(input_value);
-    viewModel();
-  } 
-  
-  if (todoModel.todoLists.length > 2) {
     //Modelにinputされたデータを格納する
     todoModel.todoLists.push(input_value);
     viewModel();
   }
 
-  if (todoModel.todoLists.length > 3) {
+  if (todoModel.todoLists.length >1 && todoModel.todoLists.length == 1) {
     //Modelにinputされたデータを格納する
     todoModel.todoLists.push(input_value);
     viewModel();
   }
 
+  if (todoModel.todoLists.length >2 && todoModel.todoLists.length == 2) {
+    //Modelにinputされたデータを格納する
+    todoModel.todoLists.push(input_value);
+    viewModel();
+  }
 
 });
 
-  //削除ボタンの作成
+//削除ボタンの作成
 
-  $(function(){
-    //let deleteclick = $("input[type='checkbox']:checked");
+$(function () {
 
-    $('.deletebutton').click(function(){
-      $("#todo_text").remove();
-    });
+  $('.deletebutton').click(function () {
+
+    todoModel.todoLists.pop();
+    viewModel();
+
   });
+  
+});
 
 
 
