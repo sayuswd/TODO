97d81bb41a855eb32b728802100bcc2dfe55abe1
sayuswd <sisi1013hola@yahoo.co.jp>
2019-04-26 16:10:48 +0900

    viewModel不具合修正完了

--- todo.js
+++ todo.js
@@ -11,17 +11,17 @@ let todoModel = new Model();
 function viewModel() {
   //HTMLに表示する機能
 
-  if (todoModel.todoLists.length > 0) {
+  if (todoModel.todoLists.length >= 1 && todoModel.todoLists.length === 1) {
     const htmlcode01 = '<label><input type="checkbox"/><span class="checkbox-icon"></span><p class="todo_text01"></p></label>';
     $("#todo_text").html(htmlcode01);
     $(".todo_text01").text(todoModel.todoLists[0]);
   }
-  if (todoModel.todoLists.length > 1) {
+  if (todoModel.todoLists.length >= 2 && todoModel.todoLists.length === 2) {
     const htmlcode02 = '<label><input type="checkbox"/><span class="checkbox-icon"></span><p class="todo_text02"></p></label>';
     $("#todo_text").append(htmlcode02);
     $(".todo_text02").text(todoModel.todoLists[1]);
   }
-  if (todoModel.todoLists.length > 2) {
+  if (todoModel.todoLists.length >= 3 && todoModel.todoLists.length === 3) {
     const htmlcode03 = '<label><input type="checkbox"/><span class="checkbox-icon"></span><p class="todo_text03"></p></label>';
     $("#todo_text").append(htmlcode03);
     $(".todo_text03").text(todoModel.todoLists[2]);
@@ -34,23 +34,26 @@ $('.addbutton').click(function () {
   let input_value = $('#textinput').val();
 
   //もし、入力されていたら表示
-  if (todoModel.todoLists.length == 0) {
+  if (todoModel.todoLists.length === 0) {
 
     //Modelにinputされたデータを格納する
     todoModel.todoLists.push(input_value);
     viewModel();
+    return;
   }
 
-  if (todoModel.todoLists.length >1 && todoModel.todoLists.length == 1) {
+  if ( todoModel.todoLists.length === 1) {
     //Modelにinputされたデータを格納する
     todoModel.todoLists.push(input_value);
     viewModel();
+    return;
   }
 
-  if (todoModel.todoLists.length >2 && todoModel.todoLists.length == 2) {
+  if (todoModel.todoLists.length === 2) {
     //Modelにinputされたデータを格納する
     todoModel.todoLists.push(input_value);
     viewModel();
+    return;
   }
 
 });
