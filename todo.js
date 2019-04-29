90c07de393f0dab9c3c73062ecfaae0771fc0fc5
sayuswd <sisi1013hola@yahoo.co.jp>
2019-04-29 23:34:57 +0900

    削除ボタン　ViewModelの実装（未）

--- todo.js
+++ todo.js
@@ -7,35 +7,57 @@ class Model {
 let todoModel = new Model();
 
 
-
 //文字を表示する　　View機能
 function viewModel() {
   //HTMLに表示する機能
 
   if (todoModel.todoLists.length >= 1 && todoModel.todoLists.length === 1) {
-    const htmlcode01 = '<label><input type="checkbox" name="check1" /><span class="checkbox-icon"></span><p class="todo_text01"></p></label>';
+    const htmlcode01 = '<div class="code01"><label><input type="checkbox" name="check1" /><span class="checkbox-icon"></span><p class="todo_text01"></p></label></div>';
     $("#todo_text").html(htmlcode01);
     $(".todo_text01").text(todoModel.todoLists[0]);
     //deletebuttonの実装01
-    const deletebutton01 = '<input type="button" class="d_button01" value="Delete"/>';
+    const deletebutton01 = '<div class="delete_button01"><input type="button" class="d_button01" value="Delete"/></div>';
     $(".deletebutton").html(deletebutton01);
   }
   if (todoModel.todoLists.length >= 2 && todoModel.todoLists.length === 2) {
-    const htmlcode02 = '<label><input type="checkbox" class="check2"/><span class="checkbox-icon"></span><p class="todo_text02"></p></label>';
+    const htmlcode02 = '<div class="code02"><label><input type="checkbox" class="check2"/><span class="checkbox-icon"></span><p class="todo_text02"></p></label></div>';
     $("#todo_text").append(htmlcode02);
     $(".todo_text02").text(todoModel.todoLists[1]);
     //deletebuttonの実装02
-    const deletebutton02 = '<input type="button" class="d_button02" value="Delete"/>';
+    const deletebutton02 = '<div class="delete_button02"><input type="button" class="d_button02" value="Delete"/></div>';
     $(".deletebutton").append(deletebutton02);
   }
   if (todoModel.todoLists.length >= 3 && todoModel.todoLists.length === 3) {
-    const htmlcode03 = '<label><input type="checkbox" name="check3"/><span class="checkbox-icon"></span><p class="todo_text03"></p></label>';
+    const htmlcode03 = '<div class="code03"><label><input type="checkbox" name="check3"/><span class="checkbox-icon"></span><p class="todo_text03"></p></label></div>';
     $("#todo_text").append(htmlcode03);
     $(".todo_text03").text(todoModel.todoLists[2]);
     //deletebuttonの実装０１
-    const deletebutton03 = '<input type="button" class="d_button03" value="Delete"/>';
+    const deletebutton03 = '<div class="delete_button03"><input type="button" class="d_button03" value="Delete"/></div>';
     $(".deletebutton").append(deletebutton03);
   }
+}
+
+//表示した文字を削除する　　View機能
+function resetviewModel() {
+  //deleteを押した時の表示機能
+    if ('input[class="d_button01"]:checked') {
+      $(".code01").empty();
+      $(".delete_button01").empty();
+    }else{
+      return;
+    }
+    if ('input[class="d_button02"]:checked') {
+      $(".code02").empty();
+      $(".delete_button02").empty();
+    }else{
+      return;
+    }
+    if ('input[class="d_button02"]:checked') {
+      $(".code03").empty();
+      $(".delete_button03").empty();
+    }else{
+      return;
+    }
 }
 
 //登録ボタンが押されたら追加
@@ -51,7 +73,6 @@ $('.addbutton').click(function () {
     viewModel();
     return;
   }
-
 
   if (todoModel.todoLists.length === 1) {
     //Modelにinputされたデータを格納する
@@ -69,18 +90,37 @@ $('.addbutton').click(function () {
 
 });
 
-//削除ボタンの作成
-
-  $(document).on('click',".d_button01",function () {
-    if (!confirm('削除しますか？')) {
-      return false;
-    } else {
-      todoModel.todoLists.splice(0,1); //1番目から1つ削除
-      viewModel();
-      return;
-    }
-  });
-
+//Model部分のデータの削除
+$('.deletebutton').on('click', ".d_button01", function () {
+  //削除ボタンの作成 01
+  if (!confirm('削除しますか？')) {
+    return false;
+  } else {
+    todoModel.todoLists.splice(0, 1); //1番目から1つ削除
+    resetviewModel();
+    return;
+  }
+});
+$('.deletebutton').on('click', ".d_button02", function () {
+  //削除ボタンの作成 02
+  if (!confirm('削除しますか？')) {
+    return false;
+  } else {
+    todoModel.todoLists.splice(1, 1); //2番目から1つ削除
+    resetviewModel();
+    return;
+  }
+});
+$('.deletebutton').on('click', ".d_button03", function () {
+  //削除ボタンの作成 03
+  if (!confirm('削除しますか？')) {
+    return false;
+  } else {
+    todoModel.todoLists.splice(2, 1); //3番目から1つ削除
+    resetviewModel();
+    return;
+  }
+});
 
 
 
